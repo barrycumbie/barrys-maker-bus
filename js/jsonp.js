8ff36d42a95ebe7be5760ef186b191f5aeafc80c
barry cumbie <barrycumbie@gmail.com>
2020-04-14 13:58:14 -0500

    switch to json

--- js/jsonp.js
+++ js/jsonp.js
@@ -6,7 +6,7 @@ showEvents({
         "map": "https://www.una.edu/international/why-choose-una/227x150xflorence-alabama.jpg.pagespeed.ic.H_q6lbu4ug.jpg"
       },
       {
-        "location": "Sheffield, AL",
+        "location": "Killen, AL",
         "date": "Aug 15",
         "map": "https://cdn.thecrazytourist.com/wp-content/uploads/2019/04/ccimage-3154916511_570cb55ebc_b.jpg"
       },
