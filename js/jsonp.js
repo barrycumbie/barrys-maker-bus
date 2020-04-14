f2835bf5fbdd7cea16a3bb72892a527726edc27f
barry cumbie <barrycumbie@gmail.com>
2020-04-14 13:24:23 -0500

    changed data in jsonp.js

--- js/jsonp.js
+++ js/jsonp.js
@@ -1,19 +1,19 @@
 showEvents({
     "events": [
       {
-        "location": "San Francisco, CA",
-        "date": "May 1",
-        "map": "http://javascriptbook.com/code/c08/img/map-ca.png"
+        "location": "Florence, AL",
+        "date": "Aug 1",
+        "map": "https://www.una.edu/international/why-choose-una/227x150xflorence-alabama.jpg.pagespeed.ic.H_q6lbu4ug.jpg"
       },
       {
-        "location": "Austin, TX",
-        "date": "May 15",
-        "map": "http://javascriptbook.com/code/c08/img/map-tx.png"
+        "location": "Sheffield, AL",
+        "date": "Aug 15",
+        "map": "https://cdn.thecrazytourist.com/wp-content/uploads/2019/04/ccimage-3154916511_570cb55ebc_b.jpg"
       },
       {
-        "location": "New York, NY",
-        "date": "May 30",
-        "map": "http://javascriptbook.com/code/c08/img/map-ny.png"
+        "location": "Tuscumbia, AL",
+        "date": "Sep 30",
+        "map": "https://cdn.thecrazytourist.com/wp-content/uploads/2019/04/ccimage-shutterstock_1223937.jpg"
       }
     ]
   });
